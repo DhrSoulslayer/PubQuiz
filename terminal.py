eea25d1a66407c4c3bb1786719571ed50293fdb9
Thijs Westerhof <thijs@soulslayer.nl>
2023-07-27 17:47:18 +0200

    undo all functions

--- terminal.py
+++ terminal.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import evdev
+import threading
 import os
 import curses
 import time
 import sys
 import random
-import pygame
 
 def assign_fun_team_names(devices):
     fun_team_names = [
@@ -16,9 +17,15 @@ def assign_fun_team_names(devices):
 
     mouse_names = {}
     for i, device in enumerate(devices):
-        if i < len(fun_team_names) and "mouse" in device.lower():
-            mouse_names[device] = fun_team_names[i]
+        if i < len(fun_team_names) and "mouse" in device.name.lower():
+            mouse_names[device.fn] = fun_team_names[i]
     return mouse_names
+
+def monitor_mouse_clicks(device, team_name, last_team, click_registered):
+    for event in device.read_loop():
+        if not click_registered[0] and event.type == evdev.ecodes.EV_KEY and event.code == evdev.ecodes.BTN_LEFT and event.value == 1:
+            last_team[0] = team_name
+            click_registered[0] = True
 
 def display_scores(stdscr, last_team, click_registered, team_scores, quiz_round):
     stdscr.clear()
@@ -38,25 +45,20 @@ def display_scores(stdscr, last_team, click_registered, team_scores, quiz_round)
 
     stdscr.addstr("\nFunctions:")
     stdscr.addstr("\n'Q' - Quit the game.")
-    stdscr.addstr("\n'R' - Reset the game.")
-    stdscr.addstr("\n'P' - Replay the current round (only if the round is completed).")
-    stdscr.addstr("\n'Enter' - Start the next round.\n")
+    stdscr.addstr("\n'R' - Reset the game.\n")
 
-    stdscr.addstr("\nScoreboard (Points):\n")
+    stdscr.addstr("\nScoreboard:\n")
     for team_name, total_score in team_scores.items():
         stdscr.addstr(f"{team_name}: {total_score} points\n")
 
-    stdscr.addstr("\nLast team that clicked: ")
+    stdscr.addstr("\nTeam that clicked first: ")
     stdscr.addstr(f"{last_team[0]}\n" if click_registered[0] else "")
 
     if quiz_round[0] == 1:
         if click_registered[0]:
             stdscr.addstr("\nPress 'Enter' to start a new round.\n")
         else:
-            stdscr.addstr("\nRound in progress. Teams can click their mice.\n")
-    elif quiz_round[0] == 2:
-        stdscr.addstr("\nRound has been completed.")
-        stdscr.addstr("\nPress 'Enter' to start the next round or 'P' to replay this round (only if completed).\n")
+            stdscr.addstr("\nRound in progress. Waiting for a team to click.....\n")
 
     # Refresh the screen only when necessary
     stdscr.refresh()
@@ -67,29 +69,40 @@ def reset_game(team_scores, click_registered, last_team, quiz_round):
     last_team[0] = None
     quiz_round[0] = 0
 
-def replay_round(click_registered, quiz_round):
-    click_registered[0] = False
-    quiz_round[0] = 1
-
 def cancel_restart_script():
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 def main(stdscr):
-    pygame.init()
-
     curses.curs_set(0)
     stdscr.timeout(0)  # Non-blocking getch
 
-    num_mice = pygame.mouse.get_count()
-    team_names = {}
-    for i in range(num_mice):
-        device_name = f"Mouse {i+1}"
-        team_names[device_name] = f"Team {i+1}"
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    team_names = assign_fun_team_names(devices)
 
+    monitors = []
     team_scores = {name: 0 for name in team_names.values()}
     last_team = [None]
-    quiz_round = [0]  # 0: Round not started, 1: Round in progress, 2: Round completed
+    quiz_round = [1]  # 0: Round not started, 1: Round in progress
     click_registered = [False]  # To track if a click has been registered in the current round
+
+    for device, name in team_names.items():
+        try:
+            monitor = evdev.InputDevice(device)
+            monitors.append((monitor, name))
+        except:
+            print(f"Failed to open device: {device}")
+
+    if not monitors:
+        print("No mice found or failed to open all devices.")
+        return
+
+    # Start a separate thread for each mouse to monitor mouse clicks
+    mouse_threads = []
+    for monitor, name in monitors:
+        mouse_thread = threading.Thread(target=monitor_mouse_clicks, args=(monitor, name, last_team, click_registered))
+        mouse_thread.daemon = True
+        mouse_thread.start()
+        mouse_threads.append(mouse_thread)
 
     while True:
         c = stdscr.getch()
@@ -98,23 +111,16 @@ def main(stdscr):
         elif c == ord('r') or c == ord('R'):  # Reset the game when 'R' key is pressed
             reset_game(team_scores, click_registered, last_team, quiz_round)
             cancel_restart_script()
-        elif c == ord('p') or c == ord('P'):  # Replay the current round when 'P' key is pressed
-            if quiz_round[0] == 2:  # Only allow replay if the round is completed
-                replay_round(click_registered, quiz_round)
 
         start_time = time.time()
         display_scores(stdscr, last_team, click_registered, team_scores, quiz_round)
 
-        # Check if the 'Enter' key was pressed to start the next round after completing a round
-        if c == 10 and quiz_round[0] == 2:
-            quiz_round[0] = 1  # Start the next round
+        # Check if the 'Enter' key was pressed to start a new round
+        if c == 10 and quiz_round[0] == 1 and click_registered[0]:
             click_registered[0] = False
-
-        # Check for mouse clicks
-        for event in pygame.event.get():
-            if quiz_round[0] == 1 and not click_registered[0] and event.type == pygame.MOUSEBUTTONDOWN:
-                last_team[0] = team_names[pygame.mouse.get_name(event.device_index)]
-                click_registered[0] = True
+            if last_team[0]:
+                team_scores[last_team[0]] += 1
+            last_team[0] = None
 
         # Refresh the screen only when necessary
         stdscr.refresh()
